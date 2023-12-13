b1e47dbb0b55690cda3619ff1def918fd9caa995
VadimSMG <vadimsmg@gmail.com>
2023-12-13 13:39:48 +0200

    Added a user_data container for isntall and launch Docker ater instance init.

--- devops-course-terraform/main.tf
+++ devops-course-terraform/main.tf
@@ -1,40 +1,54 @@
 terraform {
   required_providers {
+    #Ресурси для сервісу AWS 
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
-    docker = {
+    #Ресурси для сервісу Docker
+/*    docker = {
       source = "kreuzwerker/docker"
       version = "~> 3.0.1"
-    }
+    }*/
   }
-
   required_version = ">= 1.2.0"
 }
-
+#Параметри провайдеру AWS
 provider "aws" {
   region = "us-west-2"
 }
-
 #Створення пари ключів для доступу через SSH
 resource "aws_key_pair" "dev_ops_test" {
   key_name = "test-key-pair"
-  public_key = file("/home/vad/Документи/DevOpsCurse/devops-course-terraform/dev-test.pub")
+  public_key = file("/home/vad/Документи/DevOpsStudyng/devops-course-terraform/dev-test.pub")
 }
-
 #Створення EC2 instance за допомогою AWS
 resource "aws_instance" "dev_ops_test" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+#Користувацький скрипт по встановленню та запуску Docker  
+  user_data = <<-EOF
+              #!bin/bash
+              sudo apt-get update -y
+              sudo apt-get install ca-certificates curl gnupg
+              sudo install -m 0755 -d /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+              sudo chmod a+r /etc/apt/keyrings/docker.gpg
+              echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update -y
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+              sudo systemctl start docker
+              EOF
 
   tags = {
     Name = "DevOpsLearning"
   }
 }
-
-#Інтеграція Docker на EC2 instance
-provider "docker" {}
+#Інтеграція Docker на EC2 instance та розгортання контейнеру серверу Nginx
+/*provider "docker" {}
 
 resource "docker_image" "nginx" {
   name 		= "nginx"
@@ -49,9 +63,8 @@ resource "docker_container" "nginx" {
 	internal = 80
 	external = 8000
   }
-}
-
-
+}*/
+#Налаштуання параметрів terraform output
 output "dev_ops_test_instance_ip" {
   value = aws_instance.dev_ops_test.public_ip
 }
