7142a54aec91736fbccf3b6e45f015279c67a78f
VadimSMG <vadimsmg@gmail.com>
2023-12-18 22:00:38 +0200

    Restructured terraform test project

---
+++ devops-course-terraform-ref/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "cidr_blocks" {
+  description = "List of CIDR for securty group config"
+  type        = list(any)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "ssh_pub_key_path" {
+  description = "Path to public SSH key"
+  type        = string
+  default     = "/home/vad/Документи/DevOpsStudyng/devops-course-terraform/dev-test.pub"
+}
+
+variable "aws_ami" {
+  description = "AWS instance ami type"
+  type        = string
+  default     = "ami-830c94e3"
+}
+
+variable "aws_instance_type" {
+  description = "AWS type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "docker_install_script" {
+  description = "Path to Docker install script"
+  type        = string
+  default     = "/home/vad/Документи/DevOpsStudyng/devops-course-terraform-ref/docker_install.sh"
+}
