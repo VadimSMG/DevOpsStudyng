8d48cdc455fa130eb5d0ab8c0424572423ca3b6f
VadimSMG <vadimsmg@gmail.com>
2023-12-27 00:15:55 +0200

    Moved EC2 instance and Security Group to Terraform modules.

--- devops-course-terraform-ref/outputs.tf
+++ devops-course-terraform-ref/outputs.tf
@@ -1,6 +1,6 @@
 output "dev_ops_instance_ip" {
   description = "Showing public IP of instance"
-  value       = try(resource.aws_instance.dev_ops_test.public_ip)
+  value       = try(module.ec2_module.aws_instance)
 }
 output "aws_account_id" {
   description = "Showing AWS ID of current account"
